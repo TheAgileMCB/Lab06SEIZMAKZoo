589d2a11b4ed4b9cd202dfd1e1c6f5d0b6dc2536
Matthew Barnhart <mcb4rnh4rt@gmail.com>
2020-05-12 16:40:31 -0500

    created Japanese abstract class

---
+++ Lab06Zoo/Lab06Zoo/MythologicalCreatures/Japanese/Japanese.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Lab06Zoo.MythologicalCreatures
+{
+    public abstract class Japanese
+    {
+        public virtual bool willMarryMe = false;
+    }
+}
