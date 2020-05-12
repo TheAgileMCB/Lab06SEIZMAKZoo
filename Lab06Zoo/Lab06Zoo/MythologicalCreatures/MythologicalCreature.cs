ff6883bd1808162afeaa08715a18de2c1845de46
Matthew Barnhart <mcb4rnh4rt@gmail.com>
2020-05-12 16:22:55 -0500

    modfied base class

--- Lab06Zoo/Lab06Zoo/MythologicalCreatures/MythologicalCreature.cs
+++ Lab06Zoo/Lab06Zoo/MythologicalCreatures/MythologicalCreature.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Text;
 
 namespace Lab06Zoo.MythologicalCreatures
@@ -7,8 +8,9 @@ namespace Lab06Zoo.MythologicalCreatures
     public abstract class MythologicalCreature
     {
         public virtual int numberOfHeads { get; set; } = 1;
-        public virtual bool hasWings { get; set; } = false;
+        public virtual bool isFriendly { get; set; } = true;
         public abstract int numberOfLegs { get; set; }
+        public abstract int numberOfWings { get; set; }
 
         
 
