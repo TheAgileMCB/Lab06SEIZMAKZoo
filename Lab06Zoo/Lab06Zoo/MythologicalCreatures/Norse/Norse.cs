fb7acf76b8453502c6867517c9f52c3c9ede1257
Matthew Barnhart <mcb4rnh4rt@gmail.com>
2020-05-12 16:42:29 -0500

    created Norse abstract class

---
+++ Lab06Zoo/Lab06Zoo/MythologicalCreatures/Norse/Norse.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Lab06Zoo.MythologicalCreatures.Norse
+{
+    public abstract class Norse
+    {
+        public virtual bool heraldsRagnarok = false;
+    }
+}
