4c9884cb49cec13a0e1992391b9e3e12de51ae58
Matthew Barnhart <mcb4rnh4rt@gmail.com>
2020-05-12 16:35:45 -0500

    made Greek class

---
+++ Lab06Zoo/Lab06Zoo/MythologicalCreatures/Greek/Greek.cs
@@ -0,0 +1,12 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Lab06Zoo.MythologicalCreatures
+{
+    public abstract class Greek : MythologicalCreature
+    {
+        public virtual bool bornOfAGod = false;
+    }
+}
