9f211cbb16e6886c57ad2083a5bfd584558bc148
Matthew Barnhart <mcb4rnh4rt@gmail.com>
2020-05-12 15:46:00 -0500

    updated IEat

--- Lab06Zoo/Lab06Zoo/IEat.cs
+++ Lab06Zoo/Lab06Zoo/IEat.cs
@@ -1,10 +1,16 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Reflection.Metadata.Ecma335;
 using System.Text;
 
 namespace Lab06Zoo
 {
-    interface Interface1
+    public interface IEat
     {
+        public abstract string Eats()
+        {
+            string eat = "";
+            return eat;
+        }
     }
 }
