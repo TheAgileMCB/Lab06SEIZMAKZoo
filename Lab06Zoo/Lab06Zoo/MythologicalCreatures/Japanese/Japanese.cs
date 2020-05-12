87371e2eee8d615df3db248a0db07a52b4fdc4da
Matthew Barnhart <mcb4rnh4rt@gmail.com>
2020-05-12 16:44:00 -0500

    updated base class reference

--- Lab06Zoo/Lab06Zoo/MythologicalCreatures/Japanese/Japanese.cs
+++ Lab06Zoo/Lab06Zoo/MythologicalCreatures/Japanese/Japanese.cs
@@ -2,9 +2,9 @@
 using System.Collections.Generic;
 using System.Text;
 
-namespace Lab06Zoo.MythologicalCreatures
+namespace Lab06Zoo.MythologicalCreatures.Japanese
 {
-    public abstract class Japanese
+    public abstract class Japanese : MythologicalCreature
     {
         public virtual bool willMarryMe = false;
     }
