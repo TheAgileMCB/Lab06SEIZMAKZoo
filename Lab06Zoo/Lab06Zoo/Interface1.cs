8265f4aa492d08b9c9ab2ca6f1cd2d566320afd2
Matthew Barnhart <mcb4rnh4rt@gmail.com>
2020-05-12 15:45:40 -0500

    wrote ICanRide

---
+++ Lab06Zoo/Lab06Zoo/Interface1.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Lab06Zoo
+{
+    interface ICanRide
+    {
+        public virtual bool CanRide()
+        {
+            //int creatureStrength = 10;
+            //if (userStrengthRoll > creatureStrength)
+            //    return true;
+            //else
+                return false;
+        }
+    }
+}
