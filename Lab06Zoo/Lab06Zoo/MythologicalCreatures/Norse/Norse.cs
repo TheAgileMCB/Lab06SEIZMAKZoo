f5a5b89af208413de641a53acf2e9d0e34451f4c
Matthew Barnhart <mcb4rnh4rt@gmail.com>
2020-05-12 21:20:32 -0500

    removed set property from abstract properties

--- Lab06Zoo/Lab06Zoo/MythologicalCreatures/Norse/Norse.cs
+++ Lab06Zoo/Lab06Zoo/MythologicalCreatures/Norse/Norse.cs
@@ -1,11 +1,20 @@
-﻿using System;
+﻿using Lab06Zoo.MythologicalCreatures.Interface;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
 namespace Lab06Zoo.MythologicalCreatures.Norse
 {
-    public abstract class Norse : MythologicalCreature
+    public abstract class Norse : MythologicalCreature, IEat, IMakeSound
     {
         public virtual bool heraldsRagnarok = false;
+
+        public abstract string Eats();
+
+        public abstract string MakeSound();
+      
     }
+
+    
+
 }
