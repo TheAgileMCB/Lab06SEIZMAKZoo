64d4bbf77821b323647ac34bdbb93e55b76693a4
Matthew Barnhart <mcb4rnh4rt@gmail.com>
2020-05-12 21:41:03 -0500

    class exhibits all current properties

--- Lab06Zoo/Lab06Zoo/MythologicalCreatures/Japanese/Kitsune.cs
+++ Lab06Zoo/Lab06Zoo/MythologicalCreatures/Japanese/Kitsune.cs
@@ -6,5 +6,21 @@ namespace Lab06Zoo.MythologicalCreatures.Japanese
 {
     public class Kitsune : Japanese
     {
+        public new int numberOfHeads = 1;
+        public new bool isFriendly = true;
+        public override int numberOfLegs => 4;
+        public override int numberOfWings => 0;
+        public new bool willMarryMe = true;
+
+
+        public override string Eats()
+        {
+            return "berries";
+        }
+
+        public override string MakeSound()
+        {
+            return "Yip!";
+        }
     }
 }
