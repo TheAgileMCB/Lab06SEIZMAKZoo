f9f092aa556b4de7dfa57357fa8decc40f775c70
Matthew Barnhart <mcb4rnh4rt@gmail.com>
2020-05-12 21:13:33 -0500

    built out creature properties

--- Lab06Zoo/Lab06Zoo/MythologicalCreatures/Norse/Sleipnir.cs
+++ Lab06Zoo/Lab06Zoo/MythologicalCreatures/Norse/Sleipnir.cs
@@ -1,10 +1,26 @@
-﻿using System;
+﻿using Lab06Zoo.MythologicalCreatures.Interface;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
 namespace Lab06Zoo.MythologicalCreatures.Norse
 {
-    public class Sleipnir : Norse
+    public class Sleipnir : Norse, IEat, IMakeSound
     {
+        public new int numberOfHeads = 1;
+        public new bool isFriendly = true;
+        public override int numberOfLegs => 8;
+        public override int numberOfWings => 0;
+        public new bool heraldsRagnarok = false;
+
+        public override string Eats()
+        {
+            return "Carrots";
+        }
+
+        public override string MakeSound()
+        {
+            return "Neight!";
+        }
     }
 }
