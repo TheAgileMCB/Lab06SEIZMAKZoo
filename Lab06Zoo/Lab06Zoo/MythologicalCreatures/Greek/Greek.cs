650c0e82452f4ac9290197c1ff12863e14891c81
Matthew Barnhart <mcb4rnh4rt@gmail.com>
2020-05-12 21:28:29 -0500

    referenced interfaces IEat and IMakeSound

--- Lab06Zoo/Lab06Zoo/MythologicalCreatures/Greek/Greek.cs
+++ Lab06Zoo/Lab06Zoo/MythologicalCreatures/Greek/Greek.cs
@@ -1,11 +1,12 @@
 ﻿
+using Lab06Zoo.MythologicalCreatures.Interface;
 using System;
 using System.Collections.Generic;
 using System.Text;
 
 namespace Lab06Zoo.MythologicalCreatures.Greek
 {
-    public abstract class Greek : MythologicalCreature
+    public abstract class Greek : MythologicalCreature, IEat, IMakeSound
     {
         public virtual bool bornOfAGod = false;
     }
--- Lab06Zoo/Lab06Zoo/MythologicalCreatures/Japanese/Japanese.cs
+++ Lab06Zoo/Lab06Zoo/MythologicalCreatures/Japanese/Japanese.cs
@@ -1,10 +1,11 @@
-﻿using System;
+﻿using Lab06Zoo.MythologicalCreatures.Interface;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
 namespace Lab06Zoo.MythologicalCreatures.Japanese
 {
-    public abstract class Japanese : MythologicalCreature
+    public abstract class Japanese : MythologicalCreature, IEat, IMakeSound
     {
         public virtual bool willMarryMe = false;
     }
