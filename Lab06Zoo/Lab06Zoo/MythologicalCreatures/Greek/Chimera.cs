90b6066fde35e0c5fd30c490840b99e066732846
Matthew Barnhart <mcb4rnh4rt@gmail.com>
2020-05-12 21:37:52 -0500

    class exhibits all current properties

--- Lab06Zoo/Lab06Zoo/MythologicalCreatures/Greek/Chimera.cs
+++ Lab06Zoo/Lab06Zoo/MythologicalCreatures/Greek/Chimera.cs
@@ -6,5 +6,20 @@ namespace Lab06Zoo.MythologicalCreatures.Greek
 {
     public class Chimera : Greek
     {
+        public new int numberOfHeads = 3;
+        public new bool isFriendly = false;
+        public override int numberOfLegs => 4;
+        public override int numberOfWings => 0;
+        public new bool bornOfAGod = false;
+
+        public override string Eats()
+        {
+            return "Humans";
+        }
+
+        public override string MakeSound()
+        {
+            return "Roar!";
+        }
     }
 }
