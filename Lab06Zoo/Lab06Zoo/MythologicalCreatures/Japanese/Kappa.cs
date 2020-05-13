167ce3a9c6e8a26c242b83ef70b4ec1f787efaa6
Matthew Barnhart <mcb4rnh4rt@gmail.com>
2020-05-12 21:42:13 -0500

    class exhibits all current properties

--- Lab06Zoo/Lab06Zoo/MythologicalCreatures/Japanese/Kappa.cs
+++ Lab06Zoo/Lab06Zoo/MythologicalCreatures/Japanese/Kappa.cs
@@ -6,5 +6,21 @@ namespace Lab06Zoo.MythologicalCreatures.Japanese
 {
     public class Kappa : Japanese
     {
+        public new int numberOfHeads = 1;
+        public new bool isFriendly = false;
+        public override int numberOfLegs => 2;
+        public override int numberOfWings => 0;
+        public new bool willMarryMe = false;
+
+
+        public override string Eats()
+        {
+            return "cucumbers";
+        }
+
+        public override string MakeSound()
+        {
+            return "???";
+        }
     }
 }
