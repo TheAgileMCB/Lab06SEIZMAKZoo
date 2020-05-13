cdf2a3ffcc9b80bf2f09ad64543c938136d57f8f
Matthew Barnhart <mcb4rnh4rt@gmail.com>
2020-05-12 21:39:32 -0500

    class exhibits all current properties

--- Lab06Zoo/Lab06Zoo/MythologicalCreatures/Greek/Pegasus.cs
+++ Lab06Zoo/Lab06Zoo/MythologicalCreatures/Greek/Pegasus.cs
@@ -6,5 +6,20 @@ namespace Lab06Zoo.MythologicalCreatures.Greek
 {
     public class Pegasus : Greek
     {
+        public new int numberOfHeads = 1;
+        public new bool isFriendly = true;
+        public override int numberOfLegs => 4;
+        public override int numberOfWings => 2;
+        public new bool bornOfAGod = true;
+
+        public override string Eats()
+        {
+            return "Carrots";
+        }
+
+        public override string MakeSound()
+        {
+            return "Neigh!";
+        }
     }
 }
